10e23cb5ff36f92d4c314b657b93da3009196797
klcatha <klcatha0518@gmail.com>
2018-12-13 19:00:22 +0900

    first commit

---
+++ src/test/kotlin/com/example/kotlinspringbootdomademo/KotlinSpringBootDomaDemoApplicationTests.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinspringbootdomademo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinSpringBootDomaDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
